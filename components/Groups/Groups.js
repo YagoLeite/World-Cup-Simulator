762fdb768adde00fc2a279752004bb9e99e9cc6d
Yago Leite <yagobatistafis@yahoo.com.br>
2022-04-04 08:45:17 -0300

    adding first file structure

---
+++ components/Groups/Groups.js
@@ -0,0 +1,14 @@
+import React from "react";
+import GroupTable from "./GroupTable";
+import GroupGames from "./GroupGames";
+
+const Groups = () => {
+  return (
+    <div>
+      <GroupTable />
+      <GroupGames />
+    </div>
+  );
+};
+
+export default Groups;
