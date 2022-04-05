ce11d17a41cf743ee6130b219c4a209432569a3b
Yago Leite <yagobatistafis@yahoo.com.br>
2022-04-05 15:08:18 -0300

    adding first bit of reducer

--- context/Reducers.js
+++ context/Reducers.js
@@ -1,7 +1,25 @@
 export const CupReducer = (state, action) => {
   switch (action.type) {
     case "SCORE-UPDATE":
-      return { ...state, [action.group]: "oi" };
+      // return {
+      //   ...state,
+      //   groupState: state.groupState.map((country) => {
+      //     if (country.name === action.payload.scoringTeam) {
+      //       return {
+      //         ...country,
+      //         goalsFor: country.goalsFor + +action.payload.value,
+      //       };
+      //     } else if (country.name === action.payload.receivingTeam) {
+      //       return {
+      //         ...country,
+      //         goalsAgainst: country.goalsAgainst - +action.payload.value,
+      //       };
+      //     } else {
+      //       return country;
+      //     }
+      //   }),
+      // };
+      return { ...state };
     case "GROUP-UPDATE":
       return { ...state, group: action.value };
     default:
