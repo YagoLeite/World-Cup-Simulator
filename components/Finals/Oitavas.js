7c8cb1b7551c8d74742f54e403b139ddfdfbf2b8
Yago Batista <yagobatistafis@yahoo.com.br>
2022-04-12 10:12:21 -0300

    adding oitava's component

---
+++ components/Finals/Oitavas.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { findingTeam, mata_mata_Handler, filteringByIndex } from "../functions";
+
+const Oitavas = () => {
+  return <div>Oitavas</div>;
+};
+
+export default Oitavas;
