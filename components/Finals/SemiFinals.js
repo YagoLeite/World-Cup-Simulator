9d6ef27f8af044354aee05bdbd10160e440f82ac
Yago Batista <yagobatistafis@yahoo.com.br>
2022-04-12 11:52:05 -0300

    updating ui

--- components/Finals/SemiFinals.js
+++ components/Finals/SemiFinals.js
@@ -1,24 +1,31 @@
 import React from "react";
-import SingleMatch from "./SingleMatch";
+import _SingleMatch from "./_SingleMatch";
+import { CupState } from "../../context/Context";
+import { HStack } from "@chakra-ui/react";
 
-const SemiFinals = (props) => {
+const SemiFinals = () => {
+  const { state, dispatch } = CupState();
+  const findSemi = (a, b) =>
+    state.groupState
+      .filter((item) => item.semi)
+      .find((item) => item.group === a || item.group === b);
   return (
-    <div>
-      <SingleMatch
+    <HStack bg="gray.100">
+      <_SingleMatch
         firstTeam={findSemi("A", "B")}
         secondTeam={findSemi("C", "D")}
         onClick={(firstTeam, secondTeam) =>
           quartasHandler(firstTeam, secondTeam, "FINAL-SELECTION")
         }
       />
-      <SingleMatch
+      <_SingleMatch
         firstTeam={findSemi("E", "F")}
         secondTeam={findSemi("G", "H")}
         onClick={(firstTeam, secondTeam) =>
           quartasHandler(firstTeam, secondTeam, "FINAL-SELECTION")
         }
       />
-    </div>
+    </HStack>
   );
 };
 
