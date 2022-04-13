d3e4abed00e1d8d8d39aac06326b657c06aa0e78
Yago Batista <yagobatistafis@yahoo.com.br>
2022-04-13 14:04:35 -0300

    fixing order of render issue

--- components/Finals/_Finals.js
+++ components/Finals/_Finals.js
@@ -7,6 +7,20 @@ import { mata_mata_Handler } from "../functions";
 const _Finals = () => {
   const { state, dispatch } = CupState();
   const findFinal = state.groupState.filter((item) => item.final);
+  const findFinalOne = state.groupState.find(
+    (item) =>
+      (item.final && item.group === "A") ||
+      (item.final && item.group === "B") ||
+      (item.final && item.group === "C") ||
+      (item.final && item.group === "D")
+  );
+  const findFinalTwo = state.groupState.find(
+    (item) =>
+      (item.final && item.group === "E") ||
+      (item.final && item.group === "F") ||
+      (item.final && item.group === "G") ||
+      (item.final && item.group === "H")
+  );
 
   return (
     <Stack
@@ -18,10 +32,11 @@ const _Finals = () => {
       <Text>Final</Text>
       <HStack w="100%" justifyContent="space-evenly">
         <_SingleMatch
+          position="winner"
           justify="space-between"
           margin="22.7%"
-          firstTeam={findFinal[0]}
-          secondTeam={findFinal[1]}
+          firstTeam={findFinalOne}
+          secondTeam={findFinalTwo}
           onClick={(firstTeam, secondTeam) =>
             mata_mata_Handler(
               firstTeam,
