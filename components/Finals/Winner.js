12a574af389bb43d8f26e76d33f8947c6abd2c57
Yago Leite <yagobatistafis@yahoo.com.br>
2022-04-19 15:17:08 -0300

    updating winner

--- components/Finals/Winner.js
+++ components/Finals/Winner.js
@@ -1,29 +1,32 @@
-import { Image, Flex } from "@chakra-ui/react";
+import { Image, Flex, Text, Box, VStack } from "@chakra-ui/react";
 import React from "react";
 import { CupState } from "../../context/Context";
 
 const Winner = () => {
   const { state } = CupState();
-  console.log(state.groupState.find((item) => item.winner));
+  const winner = state.groupState.find((item) => item.winner);
 
   return (
     <div>
       {state.groupState.find((item) => item.winner) !== undefined && (
-        <Flex
-          borderRadius="50%"
-          overflow="hidden"
-          w="300px"
-          h="300px"
-          boxShadow="lg"
-          borderColor="#c9c9c9"
-          borderWidth="1px"
-        >
-          <Image
-            objectFit="cover"
-            alt="Winners flag"
-            src={state.groupState.find((item) => item.winner).flag}
-          />
-        </Flex>
+        <VStack justifyContent="center" alignItems="center">
+          <Text>The winner is {winner.name}</Text>
+          <Flex
+            borderRadius="50%"
+            overflow="hidden"
+            w="300px"
+            h="300px"
+            boxShadow="lg"
+            borderColor="#c9c9c9"
+            borderWidth="1px"
+          >
+            <Image
+              objectFit="cover"
+              alt="Winners flag"
+              src={state.groupState.find((item) => item.winner).flag}
+            />
+          </Flex>
+        </VStack>
       )}
     </div>
   );
