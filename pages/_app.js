13e1855eec52b990ccbeceb10dc9d21338ab1742
Yago Leite <yagobatistafis@yahoo.com.br>
2022-05-03 12:33:28 -0300

    fontweight

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,11 @@ import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import CupContext from "../context/Context";
 import theme from "../components/theme";
+import "@fontsource/Oswald/200.css";
+import "@fontsource/Oswald/300.css";
 import "@fontsource/Oswald/400.css";
+import "@fontsource/Oswald/500.css";
+import "@fontsource/Oswald/600.css";
 
 function MyApp({ Component, pageProps }) {
   return (
