b0fc992b2b1c171cd1c44f15a2b6551bb2c265ff
Yago Batista <yagobatistafis@yahoo.com.br>
2022-05-06 18:44:40 -0300

    fixing position

--- components/Groups/Groups.js
+++ components/Groups/Groups.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import GroupTable from "./GroupTable";
 import _GroupTable from "./_GroupTable";
-import { Button, Grid, useToast } from "@chakra-ui/react";
+import { Button, Grid, useToast, VStack } from "@chakra-ui/react";
 import { CupState } from "../../context/Context";
 
 const groups = ["A", "B", "C", "D", "E", "F", "G", "H"];
@@ -16,16 +16,20 @@ const Groups = () => {
   const toast = useToast();
 
   return (
-    <Grid
-      px={["10px", "50px", "140px"]}
-      pt={["15px", "36px"]}
-      gridTemplateColumns="repeat(auto-fit, minmax(330px, 1fr))"
-      gap={["95px"]}
-      maxW="1440px"
-    >
-      {a &&
-        groups.map((group, index) => <_GroupTable key={index} group={group} />)}
-    </Grid>
+    <VStack>
+      <Grid
+        px={["10px", "50px", "140px"]}
+        pt={["15px", "36px"]}
+        gridTemplateColumns="repeat(auto-fit, minmax(330px, 1fr))"
+        gap={["95px"]}
+        maxW="1440px"
+      >
+        {a &&
+          groups.map((group, index) => (
+            <_GroupTable key={index} group={group} />
+          ))}
+      </Grid>
+    </VStack>
   );
 };
 
