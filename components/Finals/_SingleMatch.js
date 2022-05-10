74d39fcf87fd1f93893cf5a01e98dce9871c127e
Yago Batista <yagobatistafis@yahoo.com.br>
2022-05-10 19:03:28 -0300

    animation to singlematch

--- components/Finals/_SingleMatch.js
+++ components/Finals/_SingleMatch.js
@@ -1,6 +1,7 @@
 import { Flex, Image, Text, Stack } from "@chakra-ui/react";
 import React, { useState, useEffect } from "react";
 import { SmallCloseIcon } from "@chakra-ui/icons";
+import { motion } from "framer-motion";
 
 const SingleMatch = (props) => {
   const [classNameTeamOne, setClassNameTeamOne] = useState("");
@@ -34,95 +35,99 @@ const SingleMatch = (props) => {
       direction="row"
       marginX={props.margin}
     >
-      <Stack pt="20px" opacity={opacityOne ? "0.5" : "1"}>
-        {props.firstTeam ? (
-          <Flex
-            w={{ md: "20px", lg: "30px", xl: "40px" }}
-            h={{ md: "20px", lg: "30px", xl: "40px" }}
-            overflow="hidden"
+      <motion.div whileHover={{ scale: 1.1 }}>
+        <Stack pt="20px" opacity={opacityOne ? "0.5" : "1"}>
+          {props.firstTeam ? (
+            <Flex
+              w={{ md: "20px", lg: "30px", xl: "40px" }}
+              h={{ md: "20px", lg: "30px", xl: "40px" }}
+              overflow="hidden"
+              cursor="pointer"
+              borderRadius="50%"
+              boxShadow="dark-lg"
+              bg="gray.500"
+              onClick={() => props.onClick(props.firstTeam, props.secondTeam)}
+              className={classNameTeamOne}
+            >
+              {props.firstTeam.flag && (
+                <Image
+                  objectFit="cover"
+                  boxShadow="lg"
+                  alt="country flag"
+                  src={props.firstTeam.flag}
+                />
+              )}
+            </Flex>
+          ) : (
+            <Flex
+              w={{ md: "20px", lg: "30px", xl: "40px" }}
+              h={{ md: "20px", lg: "30px", xl: "40px" }}
+              overflow="hidden"
+              cursor="pointer"
+              borderRadius="50%"
+              bg="gray.500"
+            ></Flex>
+          )}
+          <Text
+            letterSpacing="1.5px"
+            textAlign="center"
+            fontWeight="600"
+            fontSize={["12px", "15px"]}
             cursor="pointer"
-            borderRadius="50%"
-            boxShadow="lg"
-            bg="gray.500"
             onClick={() => props.onClick(props.firstTeam, props.secondTeam)}
-            className={classNameTeamOne}
           >
-            {props.firstTeam.flag && (
-              <Image
-                objectFit="cover"
-                boxShadow="lg"
-                alt="country flag"
-                src={props.firstTeam.flag}
-              />
-            )}
-          </Flex>
-        ) : (
-          <Flex
-            w={{ md: "20px", lg: "30px", xl: "40px" }}
-            h={{ md: "20px", lg: "30px", xl: "40px" }}
-            overflow="hidden"
-            cursor="pointer"
-            borderRadius="50%"
-            bg="gray.500"
-          ></Flex>
-        )}
-        <Text
-          letterSpacing="1.5px"
-          textAlign="center"
-          fontWeight="600"
-          fontSize={["12px", "15px"]}
-          cursor="pointer"
-          onClick={() => props.onClick(props.firstTeam, props.secondTeam)}
-        >
-          {props.firstTeam?.name.slice(0, 3).toUpperCase()}
-        </Text>
-      </Stack>
+            {props.firstTeam?.name.slice(0, 3).toUpperCase()}
+          </Text>
+        </Stack>
+      </motion.div>
       <Flex alignItems="center" justifyContent="center">
         <SmallCloseIcon />
       </Flex>
-      <Stack pt="20px" opacity={opacityTwo ? "0.5" : "1"}>
-        {props.secondTeam ? (
-          <Flex
-            w={{ md: "20px", lg: "30px", xl: "40px" }}
-            h={{ md: "20px", lg: "30px", xl: "40px" }}
-            overflow="hidden"
+      <motion.div whileHover={{ scale: 1.1 }}>
+        <Stack pt="20px" opacity={opacityTwo ? "0.5" : "1"}>
+          {props.secondTeam ? (
+            <Flex
+              w={{ md: "20px", lg: "30px", xl: "40px" }}
+              h={{ md: "20px", lg: "30px", xl: "40px" }}
+              overflow="hidden"
+              cursor="pointer"
+              borderRadius="50%"
+              boxShadow="dark-lg"
+              bg="gray.500"
+              className={classNameTeamTwo}
+              onClick={() => props.onClick(props.secondTeam, props.firstTeam)}
+            >
+              {props.secondTeam.flag && (
+                <Image
+                  alt="country flag"
+                  objectFit="cover"
+                  boxShadow="lg"
+                  src={props.secondTeam.flag}
+                />
+              )}
+            </Flex>
+          ) : (
+            <Flex
+              w={{ md: "20px", lg: "30px", xl: "40px" }}
+              h={{ md: "20px", lg: "30px", xl: "40px" }}
+              overflow="hidden"
+              cursor="pointer"
+              borderRadius="50%"
+              bg="gray.500"
+            ></Flex>
+          )}
+          <Text
+            letterSpacing="1.5px"
+            textAlign="center"
+            fontWeight="600"
+            fontSize={["12px", "15px"]}
             cursor="pointer"
-            borderRadius="50%"
-            boxShadow="lg"
-            bg="gray.500"
-            className={classNameTeamTwo}
             onClick={() => props.onClick(props.secondTeam, props.firstTeam)}
           >
-            {props.secondTeam.flag && (
-              <Image
-                alt="country flag"
-                objectFit="cover"
-                boxShadow="lg"
-                src={props.secondTeam.flag}
-              />
-            )}
-          </Flex>
-        ) : (
-          <Flex
-            w={{ md: "20px", lg: "30px", xl: "40px" }}
-            h={{ md: "20px", lg: "30px", xl: "40px" }}
-            overflow="hidden"
-            cursor="pointer"
-            borderRadius="50%"
-            bg="gray.500"
-          ></Flex>
-        )}
-        <Text
-          letterSpacing="1.5px"
-          textAlign="center"
-          fontWeight="600"
-          fontSize={["12px", "15px"]}
-          cursor="pointer"
-          onClick={() => props.onClick(props.secondTeam, props.firstTeam)}
-        >
-          {props.secondTeam?.name.slice(0, 3).toUpperCase()}
-        </Text>
-      </Stack>
+            {props.secondTeam?.name.slice(0, 3).toUpperCase()}
+          </Text>
+        </Stack>
+      </motion.div>
     </Stack>
   );
 };
